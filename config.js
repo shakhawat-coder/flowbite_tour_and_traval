6a31affc65bafd548b7607f1a3ee97c35d2930eb
shakhawat <shakhawathossen188@gmail.com>
2023-08-09 01:38:52 +0600

    navbar design

---
+++ config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            navbg:'rgba(0,0,0,.2)',
+          clifford: '#da373d',
+        },
+        maxWidth: {
+            'container':"1170px",
+          },
+          backgroundImage: {
+            'bannerbg': "url('./images/banner.jpg')",
+          },
+      }
+    }
+  }
