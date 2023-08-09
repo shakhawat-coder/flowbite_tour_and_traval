d79c45cb20f4197d3c50575ecf762cacb86117f1
shakhawat <shakhawathossen188@gmail.com>
2023-08-09 10:22:13 +0600

    banner section design

--- config.js
+++ config.js
@@ -2,14 +2,18 @@ tailwind.config = {
     theme: {
       extend: {
         colors: {
-            navbg:'rgba(0,0,0,.2)',
-          clifford: '#da373d',
+            navbg:'rgba(0,0,0,.4)',
+            bnnercolor:'rgba(67,142,222,.5)',
         },
         maxWidth: {
             'container':"1170px",
           },
           backgroundImage: {
             'bannerbg': "url('./images/banner.jpg')",
+          },
+          fontFamily: {
+            nunito: "'Nunito', sans-serif;",
+            opensans: "'Open Sans', sans-serif;",
           },
       }
     }
