59cf29098571d502333e4fccd0048af444e55de9
shakhawat <shakhawathossen188@gmail.com>
2023-08-09 11:12:48 +0600

    find tour section design

--- config.js
+++ config.js
@@ -4,12 +4,14 @@ tailwind.config = {
         colors: {
             navbg:'rgba(0,0,0,.4)',
             bnnercolor:'rgba(67,142,222,.5)',
+            graytext:'#5f5f5f'
         },
         maxWidth: {
             'container':"1170px",
           },
           backgroundImage: {
             'bannerbg': "url('./images/banner.jpg')",
+            'findtourbg': "url('./images/findtourbg.jpg')",
           },
           fontFamily: {
             nunito: "'Nunito', sans-serif;",
